50f8010c68c6a4af5feaf0e2f7b944e6cebefaba
Chris Diamand <chris@diamand.org>
2013-12-14 22:29:28 +0000

    LaTeX projects can now specify a different output name.

--- targets/LaTeX.py
+++ targets/LaTeX.py
@@ -1,20 +1,24 @@
 import os
 
 class Target: # LaTeX
-    def __init__(self, src):
+    def __init__(self, src, out = None):
         assert type(src) == str
         self.src = src
-        self.out = os.path.splitext(src)[0] + ".pdf"
+        if out == None:
+            self.out = os.path.splitext(src)[0] + ".pdf"
+        else:
+            self.out = out
 
     def gen(self, fp):
+        fp.write(self.out + ": " + self.src + " | Build\n")
+        fp.write("\t@echo PDFLATEX $<\n")
+        fp.write("\t@pdflatex -output-directory " + builddir +
+                 " -halt-on-error $< > /tmp/pdflatex_log.txt" +
+                 " || cat /tmp/pdflatex_log.txt\n")
+        fp.write("\t@cp " + builddir + "/$(<:.tex=.pdf) " + self.out + "\n\n")
+
         fp.write("REMOVE_FILES += " + self.out + "\n\n")
         return self.out
 
 def write_rule(fp):
-    fp.write("%.pdf: %.tex | Build\n")
-    fp.write("\t@cp $< " + builddir + "/\n")
-    fp.write("\t@echo PDFLATEX $<\n")
-    fp.write("\t@cd " + builddir +
-             " && ( pdflatex -halt-on-error $< > /tmp/pdflatex_log.txt" +
-             " || cat /tmp/pdflatex_log.txt )\n")
-    fp.write("\t@cp " + builddir + "/$@ .\n\n")
+    None
