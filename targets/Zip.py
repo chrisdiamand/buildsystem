3490580539d0f07860502d4e56dd62a8877920b0
Chris Diamand <chris@diamand.org>
2013-12-18 00:20:36 +0000

    Can now generate zip archives.

---
+++ targets/Zip.py
@@ -0,0 +1,25 @@
+import os
+
+class Target: # Zip
+    def __init__(self, fname, contents):
+        assert type(fname) == str
+        assert type(contents) == list
+        self.fname = fname
+        self.contents = contents
+
+    def gen(self, fp):
+        fp.write(self.fname + ":")
+        for c in self.contents:
+            fp.write(" " + c)
+        fp.write("\n")
+        fp.write("\t@echo ZIP " + self.fname + "\n")
+        fp.write("\t@zip --quiet " + self.fname)
+        for c in self.contents:
+            fp.write(" " + c)
+        fp.write("\n\n")
+
+        fp.write("REMOVE_FILES += " + self.fname+ "\n\n")
+        return self.fname
+
+def write_rule(fp):
+    None
